3a424e288be1eb2c98b9d10f1464d1aec54976c7
Tomasz Olczak <tolczak@gmail.com>
2022-12-17 16:03:14 +0100

    multithreaded populate!

--- src/generators.jl
+++ src/generators.jl
@@ -7,18 +7,19 @@ function config_model(degs::Vector{T}) where T<:Integer
     mn, mx = extrema(degs)
     @assert mn > 0 && mx <= n
 
-    @info "generate half-edges"
+    @info "preparing half-edges"
+    # generate colptr
+    colptr = similar(degs, n + 1)
+    colptr[1] = 1; copyto!(colptr, 2, degs)
+    cumsum!(colptr, colptr)
+    @assert @inbounds colptr[end] == s + 1
     rows = similar(degs, s)
-    @time populate!(degs, rows)
+    @time _mt_populate!(rows, colptr)
     @info "generate permutation"
     @time perm = mt_randperm!(similar(rows))
 
     @info "generate matching"
     @time matching(rows, perm)
-    # generate colptr
-    colptr = similar(degs, length(degs)+1)
-    colptr[1] = 1; copyto!(colptr, 2, degs)
-    cumsum!(colptr, colptr)
     @info "creating graph"
     @time m = SparseMatrixCSC(n, n, colptr, rows, ones(Int8, length(rows)));
     @info "sorting"
@@ -30,18 +31,14 @@ end
 function matching(rows::Vector{T}, perm::Vector{T}) where T<:Integer
     @inbounds @threads :static for i in 1:2:length(perm)
         # p = perm[i] + perm[i+1]
-        local p, q = perm[i], perm[i+1]
+        @inbounds local p, q = perm[i], perm[i+1]
         @inbounds rows[p], rows[q] = rows[q], rows[p]
     end
 end
 
-# function populate!(degs::Vector{T}, dest<:AbstractArray{T,N}) where {T<:Integer, N<:Integer}
-function populate!(degs::AbstractArray{T}, dest::AbstractArray{T}) where {T<:Integer}
-    @assert sum(degs) == length(dest) "sum(degs) $(sum(degs)) != length(dest) $(length(dest))"
-    s,e = 0,0
-    @inbounds for (i,d) in enumerate(degs)
-        d==0 && continue
-        s,e = e+1,e+d
+function _mt_populate!(dest::AbstractArray{T}, colptr::AbstractArray{T}) where {T<:Integer}
+    @threads :static for i in 1:length(colptr)-1
+        @inbounds local s,e = colptr[i],colptr[i+1]-1
         @inbounds dest[s:e] .= i
     end
 end
